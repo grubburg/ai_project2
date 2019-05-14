9e3307a3d918bf4729afd20ff97ad28be2281e1d
Asil Mian <asilmian@gmail.com>
2019-05-14 12:44:06 +1000

    linear_player package

---
+++ linear_player/emptyStrat.py
@@ -0,0 +1,17 @@
+from linear_player.hexagon import *
+from linear_player.player import *
+
+FINISHING_HEXES = {
+    "red": {(3,-3), (3,-2), (3,-1), (3,0)},
+    "green": {(-3,3), (-2,3), (-1,3), (0,3)},
+    "blue": {(-3,0),(-2,-1),(-1,-2),(0,-3)},
+}
+
+class Strategy:
+    def __init__(self):
+        return
+
+
+    def get_next_move(self, player):
+        move = ("PASS", None)
+        return move
---
+++ linear_player/hexagon.py
@@ -0,0 +1,27 @@
+import numpy
+
+MOVE_ACTIONS = [(0, 1), (1, 0), (1, -1), (0, -1), (-1, 0), (-1, 1)]
+
+class Hexagon:
+    def __init__(self, position, occupant):
+        self.positon = tuple(position)
+        self.occupant = occupant
+        self.neighbours = self.create_neightbours()
+
+
+    def create_neightbours(self):
+        neighbour_list = []
+        for move in MOVE_ACTIONS:
+            new_tile = numpy.add(self.positon, move)
+            if is_valid_position(new_tile):
+                neighbour_list.append(tuple(new_tile))
+        return neighbour_list
+
+def is_valid_position(positon) ->bool:
+    ran = range(-3, +3 + 1)
+    if -positon[0] -positon[1] in ran and positon[0] in ran and positon[1] in ran:
+        return True
+    else:
+        return False
+
+
